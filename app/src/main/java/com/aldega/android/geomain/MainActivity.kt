636cbe5b2b1734cc9babbdfd4f14e5ef98597142
AnyukhinArtem <Anyukhin.A.Yur@sberbank.ru>
2021-07-28 23:30:40 +0300

    Глава 1

---
+++ app/src/main/java/com/aldega/android/geomain/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.aldega.android.geomain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                R.string.true_button,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        falseButton.setOnClickListener {
+            Toast.makeText(this, R.string.false_button, Toast.LENGTH_SHORT).show()
+
+
+
+        }
+
+
+    }
+}
